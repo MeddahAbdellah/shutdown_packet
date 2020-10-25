9b71d779190f1fee0854817f068f289f3554002c
Abdallah Meddah <meddah.abdellah.spcx@gmail.com>
2020-10-25 18:00:21 +0100

    WIP

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 var wol = require("wake_on_lan");
 var dgram = require("dgram");
-var net  = require('net');
 var server = dgram.createSocket("udp4");
 var MAC = process.argv.slice(2)[0];
-var magic_packet = wol.createMagicPacket(MAC);
+const { exec } = require("child_process");
 
 server.on("listening", function () {
 	var address = server.address();
     console.log("UDP Server listening on " + address.address + ":" + address.port);
-    shutdownPc();
 });
 
 server.on("message", function (message, remote) {
     console.log("MESSAGE", message.toString());
 	if(message.toString() == MAC) {
-		console.log("shutdown");
+        console.log("shutdown");
+        exec(`shutdown -s -t 0`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
     }
 });
-function shutdownPc() {
-    socket = dgram.createSocket(net.isIPv6('255.255.255.255') ? 'udp6' : 'udp4');
-    socket.once('listening', function() {
-        socket.setBroadcast(true);
-    });
-    socket.send(MAC, 0, MAC.length, 9, '255.255.255.255');
-}
+
 server.bind("9", "0.0.0.0");
